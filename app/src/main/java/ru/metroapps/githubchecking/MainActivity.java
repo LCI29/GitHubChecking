7bf7895c2ebc4616f7325810e814b9f42f94b04f
LCI29 <blind-mind@yandex.ru>
2020-05-22 01:35:25 +0300

    Added abracadabra

--- app/src/main/java/ru/metroapps/githubchecking/MainActivity.java
+++ app/src/main/java/ru/metroapps/githubchecking/MainActivity.java
@@ -11,4 +11,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    dfklsajgksaljgl
+    agfgkkfl;adjgk;ljsdfg
+    agfadgkldfk;gkdsf
+
+    agkfl;gds
+
+    fga
+            g
+    fdg
+                    fd
+    gfd
+                            sg
+    d
+                                    gf
+    ds
+
+
+
+                                            gds
+    h
+                                                    gh
+    fh
+                                                            dfjdhj
 }
